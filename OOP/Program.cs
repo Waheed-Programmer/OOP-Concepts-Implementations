7daea56a48e163d5d4bc7f7da9ff598f667262a8
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-05-09 14:25:45 +0500

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class student
+    {
+        public string Name;
+        public int age;
+        public int marks;
+
+
+        public void details()
+        {
+            Console.WriteLine("Name={0}","Age={1}","Marks={2}",Name,age,marks); 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
