bf8c215bc8ab30475eef388a2782dc40c091a373
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-10-29 17:07:11 +0500

    Addition encapsulation

---
+++ OOP/Encapsulations/Encapsulation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Encapsulations
+{
+    class Encapsulation
+    {
+        class Banksystem
+        {
+
+        }
+        static void Main(string [] arg)
+        {
+
+        }
+    }
+}
