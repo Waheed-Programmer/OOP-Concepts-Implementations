8675d45d3117a18598bf47a1397fd5309998f213
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-10-30 21:30:51 +0500

    Add a interface folder for implementation of interface

---
+++ OOP/Interface/InterfaceClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interface
+{
+    interface ISale {
+        void sale();
+    
+    }
+
+    class Acount : ISale
+    {
+        public void sale()
+        {
+            Console.WriteLine("This is interface method in derived class");
+        }
+
+    }
+
+
+
+    class InterfaceClass
+    {
+        static void Main(string [] inter)
+        {
+            Acount a = new Acount();
+            a.sale();
+            Console.ReadKey();
+
+        }
+             
+    }
+}
