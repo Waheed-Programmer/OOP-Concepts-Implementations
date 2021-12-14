a12d9021606b05a6fe750f23683957515e2cad1f
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-12-14 17:47:29 +0500

    Create a db manager class to handle multi instance are not makking

---
+++ OOP/SingletonClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class DBManager
+    {
+        //Singleto is used for only one instance create for class
+
+        private static DBManager myobject;
+        private int i = 0;
+
+    }
+}
