5af4d812bd6c301ad1e6d98f411bea067abf55ae
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-12-18 18:03:45 +0500

    Create a class for get and set property set up in code

---
+++ OOP/GetOrSetProperty.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Student
+    {
+        private int _Id;
+        private string name;
+
+        public int StdId
+        {
+            get
+            {
+                return this._Id;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Negative value and 0 not allowed");
+                }
+                else
+                {
+                    this._Id = value;
+                }
+            }
+        }
+    }
+    class GetOrSetProperty
+    {
+        public static void Main(string[] arg)
+        {
+
+        }
+    }
+}
