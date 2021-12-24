44cdbb62f2aadf778af3a59320bd958fccab0136
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-12-24 15:17:14 +0500

    make different method for caluclations

--- OOP/Interface/Implementations.cs
+++ OOP/Interface/Implementations.cs
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 namespace OOP.Interface
 {
 
+    interface calc1
+    {
+        int add(int a, int b);
+    }
+    interface calc2
+    {
+        int sub(int c, int d);
+    }
+    interface calc3
+    {
+        int mul(int e, int f);
+    }
 
+    class caluclation: calc1,calc2,calc3
+    {
+        int result1;
+        public int add(int a, int b)
+        {
+            result1 = a + b;
+            return result1;
+        }
+    }
 
 
 
