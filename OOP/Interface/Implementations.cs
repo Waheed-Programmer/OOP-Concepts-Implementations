59ae3c1c33a57366d2654530ba0c3a49adbdf083
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-12-24 15:22:48 +0500

    Implementions is complete and show results

--- OOP/Interface/Implementations.cs
+++ OOP/Interface/Implementations.cs
@@ -22,19 +22,19 @@ namespace OOP.Interface
 
     class caluclation: calc1,calc2,calc3
     {
-        int result1;
+        public int result1;
         public int add(int a, int b)
         {
             result1 = a + b;
             return result1;
         }
-        int result2;
+        public int result2;
         public int sub(int c, int d)
         {
             result2 = c - d;
             return result2;
         }
-        int result3;
+        public int result3;
         public int mul(int e, int f)
         {
             result3 = e * f;
@@ -54,6 +54,13 @@ namespace OOP.Interface
     {
         public static void Main()
         {
+            caluclation c = new caluclation();
+            c.add(5, 9);
+            c.sub(9, 5);
+            c.mul(6, 6);
+            Console.WriteLine("Additions of \n" +c.result1);
+            Console.WriteLine("Sub of \n" + c.result2);
+            Console.WriteLine("Mul of \n" + c.result3);
 
         }
     }
