2fe70b58929625f0a7ba156e587e786a86c8036e
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-12-24 18:33:55 +0500

    Implementions od abstruction using different class inherit with abstruct class

---
+++ OOP/Abstraction/AbstImpliment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Abstraction
+{
+    public abstract class shape
+    {
+        public abstract void draw();
+    }
+    class Rectangular : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("The method is Rectangular"); 
+        }
+    }
+    class Triangle : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("The method is Triangle"); 
+
+        }
+    }
+    class AbstImpliment
+    {
+        static void Main(string[] arg)
+        {
+            shape s;
+            s = new Rectangular();
+            s.draw();
+
+
+
+
+        }
+    }
+}
