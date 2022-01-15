06846194e2c23b63ab85652076f08ae946688e76
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-01-15 18:42:06 +0500

    Successfully run code

--- OOP/Delegate.cs
+++ OOP/Delegate.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
+    public delegate void Di(string v, string u);
+
     class Delegate
     {
+        public static void seti(string q, string w)
+        {
+            Console.WriteLine("{0}{1}", q, w);
+        }
+        static void Main(string[] it)
+        {
+            Di d = new Di(Delegate.seti);
+            d.Invoke("Waheed", "Arshad");
+            Console.ReadKey();
+        }
     }
 }
